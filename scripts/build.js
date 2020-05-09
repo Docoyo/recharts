b9c51534d3106a3297cdb5114c0eb9f8b944db89
Nicolaj Kirchhof <nico@docoyo>
2020-05-10 01:41:16 +0200

    Fixed install process by using customized script

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,12 @@
-var execSync = require('child_process').execSync;
-var readFileSync = require('fs').readFileSync;
+const { execSync } = require('child_process');
+const fs = require('fs');
 
+if (!fs.existsSync(`${__dirname}/../.buildme`)) {
+  console.log('Skipping build because install from npm');
+  return;
+}
+
+console.log('Installing because install is from repo');
 function exec(command) {
   execSync(command, { stdio: [0, 1, 2] });
 }
